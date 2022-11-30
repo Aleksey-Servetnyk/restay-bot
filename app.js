92e67d1c896e0dda71a68b470d2dc7894b87a532
ADMIN <alex.servetnyk@gmail.com>
2022-11-30 12:31:50 +0100

    localization continued: change locale buttons added

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express(),
 
 app.use(morgan('dev'))
 
-const {Telegraf} = require('telegraf')
+const {Telegraf, Markup, Extra} = require('telegraf')
 const TelegrafI18n = require('telegraf-i18n')
 const path = require("path");
 
@@ -29,15 +29,57 @@ const i18n = new TelegrafI18n({
     directory: path.resolve(__dirname, 'locales')
 })
 
+
+const buttons = Extra.markup(
+    Markup.inlineKeyboard(
+        [
+            Markup.callbackButton('English', 'bE', false),
+            Markup.callbackButton('Russian', 'bR', false)
+        ]
+    )
+)
+
+let LANG = ''
+
 //app.use(Telegraf.memorySession())
 bot.use(i18n.middleware())
 i18n.middleware(locale = 'ru')
-bot.start((ctx) => {
+
+
+bot.start(async (ctx) => {
     //ctx.i18n.locale('ru')
-    ctx.reply(ctx.i18n.t('hello', {ctx}))
+    //await ctx.reply(ctx.i18n.t('hello', {ctx}))
+    await ctx.reply('Can you choose language?', buttons)
 })
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+
+addBtnAction('bE')
+addBtnAction('bR')
+
+
+/**
+ * Language switching function
+ * @param id_btn id language button
+ */
+function addBtnAction(id_btn) {
+    bot.action(id_btn, async (ctx) => {
+        switch (id_btn) {
+            case 'bE':
+                ctx.i18n.locale('en')
+                await ctx.reply(ctx.i18n.t('hello', {ctx}))
+                break
+            case 'bR':
+                ctx.i18n.locale('ru')
+                await ctx.reply(ctx.i18n.t('hello', {ctx}))
+                break
+            default:
+                break
+        }
+    })}
+
+
 bot.launch().then(r => {
 })
+

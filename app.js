c525d795bdc60e838cfb77f43d494e3cff20f4d8
ADMIN <alex.servetnyk@gmail.com>
2022-11-30 11:26:53 +0100

    localization continued: change locale case added

--- app.js
+++ app.js
@@ -7,20 +7,37 @@ const app = express(),
 
 app.use(morgan('dev'))
 
-const { Telegraf } = require('telegraf')
+const {Telegraf} = require('telegraf')
 const TelegrafI18n = require('telegraf-i18n')
 const path = require("path");
 
+/*
 const i18n = new TelegrafI18n({
     defaultLanguage: 'en',
     allowMissing: false, // Default true
     directory: path.resolve(__dirname, 'locales')
 })
+*/
+
+//i18n.loadLocale('en', {greeting: 'Hello!'})
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
+
+const i18n = new TelegrafI18n({
+    useSession: false,
+    defaultLanguageOnMissing: false, // implies allowMissing = true
+    directory: path.resolve(__dirname, 'locales')
+})
+
+//app.use(Telegraf.memorySession())
 bot.use(i18n.middleware())
-bot.start((ctx) => ctx.reply(ctx.i18n.t('hello', {ctx})))
+i18n.middleware(locale = 'ru')
+bot.start((ctx) => {
+    //ctx.i18n.locale('ru')
+    ctx.reply(ctx.i18n.t('hello', {ctx}))
+})
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.launch().then(r => {})
+bot.launch().then(r => {
+})
